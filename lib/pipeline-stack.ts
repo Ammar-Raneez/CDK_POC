76a3064c6b3feefa2af890c58d3e970589767932
ammar <ammarraneez@gmail.com>
2022-01-30 21:26:52 +0530

    feat: revert to old pipeline config

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,10 +1,9 @@
-import { Stack } from 'aws-cdk-lib';
-import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { SecretValue, Stack } from 'aws-cdk-lib';
+import { BuildSpec, LinuxBuildImage, PipelineProject } from 'aws-cdk-lib/aws-codebuild';
+import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
+import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { Construct } from 'constructs';
 import { POCStackProps } from '../resources/lib/poc-stack.model';
-
-const REGION = process.env.REGION;
-const ACCOUNT = process.env.ACCOUNT;
 
 interface PipelineStackProps extends POCStackProps {
   codeStarConnection: string;
@@ -14,17 +13,47 @@ export class PipelineStack extends Stack {
   constructor(scope: Construct, id: string, props: PipelineStackProps) {
     super(scope, id, props);
 
-    const pipeline = new CodePipeline(this, `CDKPOCPipeline-${props.stageName}`, {
+    const pipeline = new Pipeline(this, `CDKPOCPipeline-${props.stageName}`, {
       pipelineName: `CDKPOCPipeline-${props.stageName}`,
       crossAccountKeys: false,
-      synth: new ShellStep(`pipeline-synth-${props.stageName}`, {
-        input: CodePipelineSource.connection('Ammar-Raneez/cdk-poc', 'main', {
-          connectionArn: `
-            arn:aws:codestar-connections:${REGION}:${ACCOUNT}:connection/${props.codeStarConnection}
-          `
-        }),
-        commands: ['npm ci', 'npm run build', 'npx cdk synth']
-      })
     });
+
+    // place stage outputs in artifacts
+    const sourceOutput = new Artifact(`source-output-${props.stageName}`);
+
+    // connect to github repo
+    pipeline.addStage({
+      stageName: `source-${props.stageName}`,
+      actions: [
+        new GitHubSourceAction({
+          owner: 'Ammar-Raneez',
+          repo: 'cdk-poc',
+          branch: 'main',
+          actionName: `Pipeline-Source-${props.stageName}`,
+          oauthToken: SecretValue.secretsManager('CDK_POC_PIPELINE'),
+          output: sourceOutput
+        })
+      ]
+    });
+
+    const buildOutput = new Artifact(`build-output-${props.stageName}`);
+
+    // build process
+    pipeline.addStage({
+      stageName: `build-${props.stageName}`,
+      actions: [
+        new CodeBuildAction({
+          input: sourceOutput,
+          outputs: [buildOutput],
+          actionName: `Pipeline-Build-${props.stageName}`,
+          project: new PipelineProject(this, `CDKPOCBuildProject-${props.stageName}`, {
+            environment: {
+              buildImage: LinuxBuildImage.STANDARD_5_0
+            },
+            buildSpec: BuildSpec.fromSourceFilename('build-spec/cdk-build-spec.yml')
+          })
+        })
+      ]
+    })
   }
 }
