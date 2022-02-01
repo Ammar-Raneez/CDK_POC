8fb9bd51dc833c3060ef23e01d8e67bb74e23cc9
ammar <ammarraneez@gmail.com>
2022-02-01 07:24:00 +0530

    feat: migrate aws regions

--- bin/cdk-poc.ts
+++ bin/cdk-poc.ts
@@ -10,25 +10,37 @@ const app = new cdk.App();
 const authStack = new AuthStack(app, 'CdkPocAuthStack', {
   stackName: 'CdkPocAuthStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 
 const mailerStack = new MailerStack(app, 'CdkPocMailerStack', {
   stackName: 'CdkPocMailerStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 
 const helloStack = new HelloStack(app, 'CdkPocHelloStack', {
   stackName: 'CdkPocHelloStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 
 const auctionStack = new AuctionStack(app, 'CdkPocAuctionStack', {
   stackName: 'CdkPocAuctionStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 
 const pipelineStack = new PipelineStack(app, 'CdkPocPipelineStack', {
@@ -38,26 +50,38 @@ const pipelineStack = new PipelineStack(app, 'CdkPocPipelineStack', {
   codeStarConnection: 'e95f7f83-2072-419f-bb2b-c8a4cd3aae61',
   env: {
     account: '634590378560',
-    region: 'us-east-1'
+    region: 'eu-west-1'
   }
 });
 pipelineStack.addServiceStack(authStack, 'Auth', {
   stackName: 'CdkPocAuthStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 pipelineStack.addServiceStack(mailerStack, 'Mailer', {
   stackName: 'CdkPocMailerStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 pipelineStack.addServiceStack(helloStack, 'Hello', {
   stackName: 'CdkPocHelloStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
 pipelineStack.addServiceStack(auctionStack, 'Auction', {
   stackName: 'CdkPocAuctionStack',
   stageName: 'dev',
-  emailSender: 'ammarraneez@gmail.com'
+  emailSender: 'ammarraneez@gmail.com',
+  env: {
+    region: 'eu-west-1'
+  }
 });
